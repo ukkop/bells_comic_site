8280674455d51141495da1aabf19744374e22617
ukkop <andypoystila22@gmail.com>
2026-01-02 11:01:47 -0800

    playing

--- js/potentia_header.js
+++ js/potentia_header.js
@@ -1,14 +1,16 @@
 //the header of the site would be handled in this javascript file, so you don't have to copypaste the whole thing onto every page.
 //at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeHeader"
-document.querySelector(".writeHeader").innerHTML = `
+document.querySelector(".potentia_header").innerHTML = `
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+
     <header align="center">
-        <a href="index.html"><img src="./img/logo.png" alt="" /></a> 
+        <a href="index.html"> <img src="/assets/img/potentia_cover_title.png" alt="Handwritten title for the webcomic POTENTIA, by Bells White/Skelebells" / width="700" > </a> 
 
         <div id="nav">
-            <a href="index.html">HOME</a> |
-            <a href="archive.html">ARCHIVE</a> |
-            <a href="about.html">ABOUT</a> |
-            <a href="characters.html">CHARACTERS</a>
+            <a href="index.html">home</a> |
+            <a href="archive.html">archive</a> |
+            <a href="about.html">about</a> |
+            <a href="characters.html">characters</a>
         </div>
     </header>
 `;
